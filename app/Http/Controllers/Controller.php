bc13171e55e64f19c2c8db5bf0a8485cdf471497
marcelop127 <34518473+marcelop127@users.noreply.github.com>
2022-10-26 16:58:16 -0300

    Update Controller.php
    Funções para facilitar o retorno de response e erros nas classes controllers.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,56 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Retorna uma response com os dados e o código http.
+     *
+     * @param  array|object  $data
+     * @param  int  $code
+     * @return \Illuminate\Http\Response
+     */
+    protected function response($data, $code=200) {
+        return response()->json($data, $code);
+    }
+
+    /**
+     * Retorna uma mensagem de erro.
+     *
+     * @param  int  $type
+     * @return string
+     */
+    protected function errorMessages($type=null) {
+        $erro_msg = '';
+
+        switch ($type) {
+            case 'update': 
+                $erro_msg = 'Não foi possível atualizar os dados.';
+                break;
+            case 'not_found': 
+                $erro_msg = 'Objeto não encontrado!';
+                break;
+            default:
+                $erro_msg = 'Ocorreu um erro inesperado. Tente novamente.';
+                break;
+        };
+
+        return $erro_msg;
+    }
+
+    /**
+     * Retorna um erro através de uma response
+     *
+     * @param  \Exception  $e
+     * @param  int  $type
+     * @param  int  $code
+     * @return \Illuminate\Http\Response
+     */
+    protected function errorResponse($e, $type=null, $code=500) {
+        $data = [
+            'msg' => $this->errorMessages($type),
+            'erro' => $e->getMessage(),
+        ];
+
+        return $this->response($data, $code);
+    }
 }
